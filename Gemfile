f535c9cf627a09261619b39269b050412083f782
thomashaenig <thomas.haenig@akquinet.de>
2020-07-24 12:53:09 +0200

    update cap and insert code to create file

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem "capistrano", "~> 3.14", require: false
+gem "capistrano-npm"
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+var fs = require('fs');
+
+
 console.log("Hello World");
+
+fs.writeFileSync("newfile.txt", "First File", () => {
+    console.log("file written");
+});
